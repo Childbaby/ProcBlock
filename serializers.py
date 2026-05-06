4b7f55ad9aad24553420ce31971293315e9b9756
Davison101 <161770158+Davison101@users.noreply.github.com>
2026-05-06 17:13:34 +0200

    chore: remove unused Django modules (celery, hashing, models, privacy, serializers, settings, solana_bridge, tasks, views)

--- serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import MedicineRecord
-
-class MedicineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MedicineRecord
-        fields = '__all__'
